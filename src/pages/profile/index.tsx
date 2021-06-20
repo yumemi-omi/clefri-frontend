dbe8f26071e9888fa3c09f0beffceb9fed0e35ea
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-06-20 16:36:29 +0900

    feat: プロフィール仮作成

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { css } from '@emotion/react';
+import { useQuery, gql } from 'urql';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const testQuery = gql`
+  query MyQuery {
+    user {
+      display_name
+      user_name
+    }
+  }
+`;
+
+const Profile: FC = () => {
+  const { loginWithRedirect, logout, user } = useAuth0();
+  const [result] = useQuery({
+    query: testQuery,
+  });
+  console.log({ result, user });
+
+  return (
+    <section>
+      <h1>サンプル</h1>
+      <button onClick={async () => await loginWithRedirect()}>ログイン</button>
+      <button onClick={() => logout({ returnTo: window.location.origin })}>
+        ログアウト
+      </button>
+    </section>
+  );
+};
+
+export default Profile;
