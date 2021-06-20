a899894675d84503c35cc7f32b03944b8c9d2f98
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-06-20 19:37:14 +0900

    fix: プロフィール取得・更新

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,36 @@
 import { VFC } from 'react';
 import { css } from '@emotion/react';
-import { useQuery, gql } from 'urql';
+import { useQuery, useMutation, gql } from 'urql';
 import { useAuth0 } from '@auth0/auth0-react';
 
-const testQuery = gql`
-  query MyQuery {
-    user {
-      display_name
+const FetchUser = gql`
+  query MyQuery($user_id: String!) {
+    user(where: { user_id: { _eq: $user_id } }) {
+      user_id
       user_name
+      display_name
+      mail
+    }
+  }
+`;
+
+const UpdateUser = gql`
+  mutation MyMutation(
+    $user_id: String = ""
+    $display_name: String = ""
+    $mail: String = ""
+  ) {
+    update_user(
+      where: { user_id: { _eq: $user_id } }
+      _set: { mail: $mail, display_name: $display_name }
+    ) {
+      affected_rows
+      returning {
+        user_id
+        user_name
+        display_name
+        mail
+      }
     }
   }
 `;
@@ -15,13 +38,55 @@ const testQuery = gql`
 const Profile: VFC = () => {
   const { loginWithRedirect, logout, user } = useAuth0();
   const [result] = useQuery({
-    query: testQuery,
+    query: FetchUser,
+    variables: {
+      user_id: user?.sub,
+    },
+    pause: !user,
   });
-  console.log({ result, user });
+  const [updateResult, updateUser] = useMutation(UpdateUser);
+
+  const submit = async () => {
+    const variables = {
+      user_id: user?.sub,
+      display_name: 'test太郎',
+      mail: 'test mail',
+    };
+    const result = await updateUser(variables);
+    console.log('update', { result });
+  };
+
+  if (!user || result.error || result.fetching || updateResult.fetching) {
+    console.log({ user, result, updateResult });
+    return <div>Loading</div>;
+  }
+
+  const my = result.data && result.data.user && result.data.user[0];
+  if (!my) {
+    return (
+      <div>
+        <div> ログインしてください</div>
+        <button onClick={async () => await loginWithRedirect()}>
+          ログイン
+        </button>
+      </div>
+    );
+  }
 
   return (
     <section>
-      <h1>サンプル</h1>
+      <h1>プロフィール</h1>
+      <div>
+        <div>id: {my.user_id}</div>
+        <div>name: {my.user_name}</div>
+        <div>mail: {my.mail}</div>
+        <div>display_name: {my.display_name}</div>
+      </div>
+      <form id="profile-form" onSubmit={submit}>
+        <button type="submit" value="更新内容送信" form="profile-form">
+          更新
+        </button>
+      </form>
       <button onClick={async () => await loginWithRedirect()}>ログイン</button>
       <button onClick={() => logout({ returnTo: window.location.origin })}>
         ログアウト
