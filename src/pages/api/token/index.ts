6558f8a74c3549dcbdabff8da99d51970c1ef880
yumemi-omi <hiro.041310402.kt@gmail.com>
2021-10-03 22:10:14 +0900

    deleted unused file related auth and urql

--- src/pages/api/token/index.ts
+++
@@ -1,6 +0,0 @@
-import auth0 from '@/lib/auth0';
-
-export default auth0.withApiAuthRequired(async function token(req, res) {
-  const session = await auth0.getSession(req, res);
-  res.status(200).json({ accessToken: session?.accessToken });
-});
